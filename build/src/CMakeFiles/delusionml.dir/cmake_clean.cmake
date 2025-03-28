c767104b7436ae8f173dd6b3b2afabaa22f6fd6b
Aman <amanvaibhavjha23@gmail.com>
2025-03-28 12:35:28 +0530

    Added minimal cmake files for cross platform development compatibility ( anyway i m trying to build mac specific architecture)

---
+++ build/src/CMakeFiles/delusionml.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/delusionml.dir/core/tensor.cpp.o"
+  "CMakeFiles/delusionml.dir/core/tensor.cpp.o.d"
+  "libdelusionml.dylib"
+  "libdelusionml.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/delusionml.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
